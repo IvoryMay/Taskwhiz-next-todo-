193ccfd71f68e2f9529c2a276debff30acbad4a9
IvoryMay <theintsandikyaw1@gmail.com>
2025-04-19 13:33:21 +0630

    update about me

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,31 +1,64 @@
 import React from "react";
-
+import { GiCheckMark } from "react-icons/gi";
+import { FaHtml5 } from "react-icons/fa6";
+import { FaCss3 } from "react-icons/fa6";
+import { FaJsSquare } from "react-icons/fa";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiNextdotjs } from "react-icons/si";
 const page = () => {
+
+  const mySkills = [
+    { skill: "HTML", level: "Advanced", icon: <FaHtml5 /> },
+    { skill: "CSS", level: "Intermediate", icon: <FaCss3 /> },
+    { skill: "JS", level: "Intermediate", icon: <FaJsSquare /> },
+    { skill: "React.js", level: "Intermediate", icon: <RiReactjsLine /> },
+    { skill: "Next.js", level: "Intermediate",icon: <SiNextdotjs /> },
+  ];
+
+  const toolKits = [
+    { tool: "Tailwind CSS", description: "A utility-first CSS framework for fast, custom designs." },
+    { tool: "Zustand", description: "A lightweight and scalable state management library for React." },
+    { tool: "SWR", description: "A React hook library for remote data fetching with caching." },
+  ];
+  
   return (
-    <div className="p-10 font-serif w-[60%] mx-auto">
+    <div className="p-10 font-serif w-[75%] mx-auto">
       <h1 className="text-xl font-bold mb-4 text-[#bc4749]">About Me</h1>
       
         <p className="font-bold text-lg text-[#89b523] mb-2">Hello, I'm Theint Sandi Kyaw, a  junior front-end developer. </p>
+        <p className=" text-[#184c35]">I created this app to learn and practice my skills.</p>
         <p className="mb-4 text-[#184c35]">
         I'm dedicated to crafting seamless user experiences and visually appealing
-        interfaces.
+        interfaces.</p>
 
-        <h3>My skills</h3>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JS</li>
-            <li>React.js</li>
-            <li>Next.js</li>
-          </ul>
-          <h3>Other toolkits</h3>
-          <ul>
-            <li>Tailwind CSS</li>
-            <li>Zustand</li>
-            <li>SWR</li>
-          </ul>
+        <div className="mb-4">
+        <h3 className="mb-2 font-semibold text-[#6c584c]">My skills</h3>
+             
+              <div className=" pl-3">
+            {mySkills.map((el, index) => (
+              <div key={index} className="grid grid-cols-2">
+                 <div className="flex gap-2">
+                  <div className="text-blue-400"> {el.icon} </div>
+                  <div className="text-[#184c35]">{el.skill}</div>
+                  </div> 
+                 <div className="text-amber-700">{el.level}</div> 
+                 </div>
+            ))}
+            </div>
+          </div>
         
-      </p>
+          <div className="mb-4">
+          <h3  className="mb-2 font-semibold text-[#6c584c]">Other tool kits</h3>
+          <ul className="pl-3 ">
+            {toolKits.map((el, index) => (
+              <li key={index} className="text-[#03045e]">
+                {el.tool}
+              </li>
+            ))}
+          </ul>
+          </div>
+        
+      
       <p className=" text-[#184c35]">
         I thrive on learning and growing as a developer, and I am eager to join
         a collaborative development team where I can expand my skills,
